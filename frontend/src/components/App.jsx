31ce681ead20b587d94729a824cd5d8d2e8d50a6
VladyBarvy <vladbarskoff@yandex.ru>
2025-06-09 17:53:21 +0300

    090625 step 11 Rollbar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -10,17 +10,6 @@ import RegisterPage from '../routes/RegisterPage.jsx';
 import "./App.css";
 import ProtectedRoute from './ProtectedRoute.jsx';
 import { useSelector } from 'react-redux';
-import { Provider, ErrorBoundary } from '@rollbar/react'; // Provider imports 'rollbar'
-
-const rollbarConfig = {
-  accessToken: '04b172f02a6548e79408a65929ffafcf',
-  environment: 'testenv',
-};
-
-function TestError() {
-  const a = null;
-  return a.hello();
-}
 
 function RootRoute() {
   const { isAuthenticated } = useSelector((state) => state.auth);
@@ -31,12 +20,6 @@ function RootRoute() {
 function App() {
   return (
     <Provider store={store}>
-      <Provider config={rollbarConfig}>
-        <ErrorBoundary>
-          <TestError />
-        </ErrorBoundary>
-      </Provider>
-
       <BrowserRouter>
         <Navigation />
         <Routes>
@@ -53,9 +36,6 @@ function App() {
             }
           />
         </Routes>
-
-
-
       </BrowserRouter>
     </Provider>
 

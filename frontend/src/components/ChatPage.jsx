6a6da75a38981a3e9984aee10394c378013225ec
VladyBarvy <vladbarskoff@yandex.ru>
2025-06-11 20:49:00 +0300

    110625 fix menu channels 11

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -646,12 +646,27 @@ const ChatPage = () => {
                   style={{ marginLeft: '8px' }} // небольшой отступ слева от кнопки меню
                 >
                   ⋮
+                  <span
+                    style={{
+                      position: 'absolute',
+                      width: 1,
+                      height: 1,
+                      padding: 0,
+                      margin: -1,
+                      overflow: 'hidden',
+                      clip: 'rect(0, 0, 0, 0)',
+                      whiteSpace: 'nowrap',
+                      border: 0,
+                    }}
+                  >
+                    Управление каналом
+                  </span>
                 </button>
               </div>
 
               {channel.removable && (
                 <div
-                  className={`channel-menu ${openMenuChannelId === channel.id ? 'visible' : 'hidden'}`}    
+                  className={`channel-menu ${openMenuChannelId === channel.id ? 'visible' : 'hidden'}`}
                   style={{
                     position: 'absolute',
                     background: 'white',
